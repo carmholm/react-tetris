0ad3420ebdf30a3916bc1bf4b55c01b5a561f082
Marie Godon <Marie@Maries-MacBook-Air.local>
2016-05-23 21:16:08 -0400

    began game play logic with single falling piece

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,138 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import constants from './constants.js';
+import Grid from './components/grid.jsx';
 
-class App extends React.Component {
-  render() {
-    return (
-        <div>
-            <h1>React Boilerplate</h1>
-            {/*Application body*/}
-        </div>
-    );
-  }
+
+function getTets(){
+	var pieces = ['I', 'J', 'Z', 'S', 'O', 'L', 'T'];
+	var randomNumber = Math.floor(Math.random()*7);
+	
+	console.log(constants.SHAPES[pieces[randomNumber]]);
+	return constants.SHAPES[pieces[randomNumber]];
 }
 
-App.propTypes = {};
-App.defaultProps = {};
+
+class App extends React.Component{
+	constructor(props){
+		super();
+		this.state={
+			grid: Grid.GetInitialGrid(constants.GRID_ROWS, constants.GRID_COLS),
+			activePiece: getTets(),
+			activePiecePosition: {
+				x: 3,
+				y: -3
+			},
+			pieceStartTime: new Date()
+		}
+	this.updateGameState=this.updateGameState.bind(this);
+	}
+	componentDidMount(){
+		this.updateGameState();
+	}
+	updateGameState(){
+		var currentState=this.state;
+		var y = currentState.activePiecePosition.y;
+		var elapsedSeconds = (new Date() - this.state.pieceStartTime)/1000;
+		//put the minus three in a constant somewhere?
+		var newY = -3 + elapsedSeconds;
+		this.state.activePiecePosition.y=newY;
+		this.setState({
+			activePiecePosition: this.state.activePiecePosition
+		})
+		requestAnimationFrame(this.updateGameState);
+	}
+// 	updateGameState() {
+//     //... code
+//     var currentState = this.state;
+    
+//     var elapsed = new Date() - this.state.startTime
+    
+//     if (this.hardDrop) {
+//       //go faster
+//     }
+//     //check if piece can go down
+//     //if not, merge with board, setState
+//     // setState ? but not always
+    
+//     requestAnimationFrame(this.updateGameState);
+//   }
+  	render() {
+    	return (
+        	<div>
+				<Grid.Grid grid={this.state.grid} activePiece={{activePiece: this.state.activePiece, activePiecePosition: this.state.activePiecePosition}} />
+        	</div>
+    	);
+  	}
+}
+
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super()
+//     this.updateGamestate= this.updateGamestate.bind(this);
+//     // do it for the others
+    
+//     this.state = {
+//       board: [
+        
+//       ],
+//       currentPiece: {
+        
+//       },
+//       startTime: new Date()
+//     }
+//   }
+//   componentDidMount() {
+//     document.addEventListener('keydown', this.keyDown);
+//     document.addEventListener('keyup', this.keyUp);
+//     //socket.on('handicap', this.handicap)
+//     this.updateGamestate();
+//   }
+  
+  
+//   keyDown(e) {
+//     if (e.which === UP){rotate()} //up
+//     if (e.which === RIGHT) {x+=1}
+//     if (e.which === DOWN) {this.hardDrop=true}
+//     // use e.which to figure out which key is down
+//     // let's say that the user presed the DOWN key
+//     // this.downKey = true;
+//   }
+  
+//   keyUp(e) {
+//     // use e.which to figure out which key is down
+//     // let's say that the user presed the DOWN key
+    
+//     // this.hardDrop = false;
+//   }
+  
+//   updateGameState() {
+//     //... code
+//     var currentState = this.state;
+    
+//     var elapsed = new Date() - this.state.startTime
+    
+//     if (this.hardDrop) {
+//       //go faster
+//     }
+//     //check if piece can go down
+//     //if not, merge with board, setState
+//     // setState ? but not always
+    
+//     requestAnimationFrame(this.updateGameState);
+//   }
+  
+//   render() {
+//     return (
+//         <div>
+//             {/*Application body*/}
+//         </div>
+//     );
+//   }
+// }
+
+// App.propTypes = {};
+// App.defaultProps = {};
 
 ReactDOM.render(<App />, document.querySelector('#app'));
---
+++ src/components/grid.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import constants from '../constants.js';
+
+function Row(props) {
+    return (
+        <div className="row">
+            {props.row.map(
+                (cell, index) => cell === 0 ? <div className="emptyCell" key={"c"+index}></div> : <div className="fullCell"></div>
+            )}
+        </div>
+    )
+}
+
+// Row(object);
+// object = {
+//     row: aRow,
+//     index: index
+// }
+
+
+function Grid(props) {
+
+    var currentX = props.activePiece.activePiecePosition.x / constants.GRID_COLS * 100;
+    var currentY = props.activePiece.activePiecePosition.y / constants.GRID_ROWS * 100;
+
+
+    return (
+        <div className="container">
+            <div className="grid">
+            {
+                props.grid.map(
+                    (row, index) => <Row row={row} key={"r"+index}/>
+                )
+            }
+            <div className="currentPiece"  style={{position: "absolute", top: currentY + "%", left: currentX + "%"}}>
+                {   
+                    props.activePiece.activePiece.map(
+                        (row, index) => <Row row={row} key={"pr"+index} />
+                    )
+                }
+            </div>
+            </div>
+
+        </div>
+    )
+}
+
+//get initial grid of 0s for any given height/width
+function GetInitialGrid(rows, cols){
+    var grid = [];
+    for (var row=0; row<rows; row++){
+        grid[row]=[];
+        for (var col=0; col<cols; col++){
+            grid[row][col] = 0;
+        }
+    }
+    return grid;
+}
+
+module.exports={
+    GetInitialGrid: GetInitialGrid,
+    Grid: Grid
+}
